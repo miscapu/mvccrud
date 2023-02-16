dbb14be9b7ed421f29fa3407965ea966367e6917
miscapu <mignan2010@outlook.com>
2023-02-16 09:07:27 -0300

    Fourth: Connect to Database miscapuCrud and table products, testing debug

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,8 @@
  * Author URI: https://miscapu.com
  */
 namespace App\Models;
+use \PDO;
+use \PDOException;
 
 class Product{
 
@@ -28,15 +30,55 @@ class Product{
 
     /**
      * PASS: password connection database
+     * @var string
      */
     const PASS  =   "root";
 
     /**
      * DBNAME: database name
+     * @var string
      */
     const DBNAME  =   "miscapuCrud";
 
 
+    /**
+     * table: table name manipulate
+     * @var string
+     */
+    private $table;
+
+    /**
+     * Instance DB PDO
+     * @var PDO
+     */
+    private $connection;
 
 
+    /**
+     * Define table and coonnection instance
+     * @param string $table
+     */
+    public function __construct( $table = null )
+    {
+        // Table property is equal to param
+        $this->table    =   $table;
+        // Start function setConnection for connect to database
+        $this->setConnection();
+    }
+
+    private function setConnection(){
+        try {
+            $this->connection   =   new PDO( 'mysql:host='.self::HOST.';dbname='.self::DBNAME, self::USER, self::PASS);
+            $this->connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+        }catch( PDOException $exception ){
+            die( "Error".$exception->getMessage() );
+        }
+    }
 }
+
+// debug
+$productModel   =   new Product( 'products' );
+echo "<pre>";
+print_r( $productModel );
+echo "</pre>";
