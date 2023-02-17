b3a0e1d50457f6df5b951167944bbb58e94e38f4
miscapu <mignan2010@outlook.com>
2023-02-17 00:34:54 -0300

    Edit Products OK

--- app/Views/Pages/Dashboard.php
+++ app/Views/Pages/Dashboard.php
@@ -5,7 +5,7 @@ include __DIR__.'/../Layouts/header.php';
 
 <!-- Table HTML for data -->
 
-<h2 class="modal-title text-center my-3"><?= isset( $title ) ? $title : ""; ?></h2>
+<h2 class="modal-title text-center my-3"><?= title ? title : ""; ?></h2>
     <table class="table">
         <thead>
         <tr>
@@ -13,6 +13,8 @@ include __DIR__.'/../Layouts/header.php';
             <th scope="col">Name</th>
             <th scope="col">Price</th>
             <th scope="col">Description</th>
+            <th scope="col">Actions</th>
+            
         </tr>
         </thead>
         <tbody>
@@ -29,6 +31,10 @@ include __DIR__.'/../Layouts/header.php';
             <td><?= $product->name; ?></td>
             <td><?= "R$ ".$product->price; ?></td>
             <td><?= $product->description; ?></td>
+            <td>
+                <a href="<?= BASE_URL."/edit.php?id=".$product->id;?>" class="btn btn-success">Edit</a>
+            </td>
+            
         </tr>
         <?php endforeach;
         endif;
--- app/Views/Pages/form.php
+++ app/Views/Pages/form.php
@@ -1,29 +1,30 @@
 <?php
-
 include __DIR__.'/../Layouts/header.php';
 ?>
 
 
-<h2 class="modal-title text-center my-3"><?= isset( $title ) ? $title : ""; ?></h2>
+<h2 class="modal-title text-center my-3"><?= title ? title : ""; ?></h2>
 
 <form method="post">
     <div class="mb-3">
         <label for="name-frm" class="form-label">Name Product</label>
-        <input type="text" name="name-frm" class="form-control" id="name-frm">
+        <input type="text" name="name-frm" class="form-control" id="name-frm" value="<?= isset( $product->name ) ? $product->name : ""; ?>">
     </div>
 
     <div class="mb-3">
         <label for="price-frm" class="form-label">Price Product</label>
-        <input type="text" name="price-frm" class="form-control" id="price-frm">
+        <input type="text" name="price-frm" class="form-control" id="price-frm" value="<?= isset( $product->price ) ? $product->price : ""; ?>">
     </div>
 
     <div class="mb-3">
         <label for="description-frm" class="form-label">Description Product</label>
-        <textarea name="description-frm" id="description-frm" cols="70" rows="10"></textarea>
+        <textarea name="description-frm" id="description-frm" cols="70" rows="10"><?= isset( $product->description ) ? $product->description : ""; ?></textarea>
     </div>
 
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+
+
 
 <?php
 include __DIR__.'/../Layouts/footer.php';
